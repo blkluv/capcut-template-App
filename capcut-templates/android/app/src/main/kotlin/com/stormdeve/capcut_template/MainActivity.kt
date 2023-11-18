d3a196cc42ed2cf6a4ace9e1522a61aed494e75c
blkluv <info@blkluv.org>
2023-11-18 13:47:47 -0500

    update

--- capcut-templates/android/app/src/main/kotlin/com/stormdeve/capcut_template/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.stormdeve.capcut_template
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- capcut-templates/lib/Utils/Images.dart
+++
@@ -1,4 +0,0 @@
-class Images {
-  static const String inAppLogo = 'images/inAppLogo.png';
-  static const String loading = 'images/loading.gif';
-}
